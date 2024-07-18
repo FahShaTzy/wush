fbb4a0ea14066de00491bbb7ad4aece0ccdf2b3d
FahShaTzy <FahShaTzy@users.noreply.github.com>
2024-07-18 10:27:55 +0700

    anyar

---
+++ dgt-edit/README.md
@@ -0,0 +1,11 @@
+//ID//
+Pengalaman Maksimal di Laptop/Komputer
+----------------------------------------------
+Hover hanya dapat diaktifkan di Laptop/Komputer
+----------------------------------------------
+
+
+//EN//
+Full experience on Laptop/Computer
+----------------------------------------------
+Hover only can be activated on computer
--- dgt-edit/js/script.js
+++ dgt-edit/js/script.js
@@ -34,18 +34,80 @@ fetch( 'https://alex21321312.github.io/devilgama-technology/index.html' )
         const about = html.querySelector( 'div.content' ).children;
         for ( i = 0; i < about.length; i -= - 1 ) document.getElementsByClassName( 'fetch1' )[ i ].textContent = about[ i ].textContent;
 
-        const pemilos = html.querySelector( 'div.product-content').children;
-        document.querySelector( 'div.product-content p' ).textContent = pemilos[ 1 ].textContent;
+        html.querySelectorAll( '.product-content' ).forEach( function ( node, index ) {
 
-        const ksn = html.getElementsByClassName( 'product-content')[ 1 ].children;
-        document.getElementsByClassName( 'product-content' )[ 1 ].children[ 1 ].textContent = ksn[ 1 ].textContent;
+            document.getElementsByClassName( 'product-content' )[ index ].children[ 1 ].textContent = node.textContent;
+            // console.log(node.textContent);
 
-        const proyek = html.getElementsByClassName( 'product-content')[ 2 ].children;
-        document.getElementsByClassName( 'product-content' )[ 2 ].children[ 1 ].textContent = proyek[ 1 ].textContent;
+        } );
+
+        html.querySelectorAll( '.product-image' ).forEach( function ( node, index ) {
+
+            document.getElementsByClassName( 'product-image' )[ index ].children[ 0 ].src =
+                ( node.children[ 0 ].src ).replace(
+                    'file:///C:/Users/fahri/Documents/dgt-edit/img/',
+                    'https://alex21321312.github.io/devilgama-technology/img/'
+                );
+
+        } )
 
 } );
 
+
 /* UNUSED, JUST DON'T HAPUS */
+
+// const url = 'https://alex21321312.github.io/devilgama-technology/index.html';
+
+// const blobToBase64 = async function ( blob ) {
+
+//     return new Promise( resolve => {
+
+//         const reader = new FileReader();
+//         reader.onload = () => resolve( reader.result );
+//         reader.readAsDataURL( blob );
+
+//     } );
+
+// }
+
+// const fetchImage =  async function ( url ) {
+
+//     const response = await fetch( url );
+//     const blob = await response.blob();
+
+//     return blob;
+
+// }
+
+// const downloadImage = async function ( url ) {
+
+//     const imageBlob = await fetchImage( url );
+//     const imageBase64 = URL.createObjectURL( imageBlob );
+
+//     console.log({imageBase64});
+
+//     const a = document.createElement( 'a' );
+//     a.style.display = 'none';
+//     document.body.appendChild( a );
+//     a.download = url.replace( /^.*[\\\/]/, '' );
+//     a.href = imageBase64;
+//     a.click();
+//     a.remove();
+
+//     downloadImage = null;
+
+// }
+
+// document.addEventListener( 'click', () => { downloadImage( url ) } );
+
+// const pemilos = html.querySelector( 'div.product-content').children;
+// document.querySelector( 'div.product-content p' ).textContent = pemilos[ 1 ].textContent;
+
+// const ksn = html.getElementsByClassName( 'product-content')[ 1 ].children;
+// document.getElementsByClassName( 'product-content' )[ 1 ].children[ 1 ].textContent = ksn[ 1 ].textContent;
+
+// const proyek = html.getElementsByClassName( 'product-content')[ 2 ].children;
+// document.getElementsByClassName( 'product-content' )[ 2 ].children[ 1 ].textContent = proyek[ 1 ].textContent;
 
 // const navbar = document.getElementById( 'navbar' );
 // const navbarNav = document.getElementById( 'navbar-nav' );
